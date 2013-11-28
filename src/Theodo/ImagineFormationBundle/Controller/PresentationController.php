9d7792da7bb08866bf942b3bba90a02c09a31386
Charles Pourcel <charlesp@theodo.fr>
2013-11-28 17:22:33 +0100

    Improve style of presentation and code excerpt, remove useless dependencies and rename sidebar into header

--- src/Theodo/ImagineFormationBundle/Controller/PresentationController.php
+++ src/Theodo/ImagineFormationBundle/Controller/PresentationController.php
@@ -10,6 +10,14 @@ use Symfony\Component\HttpFoundation\Request;
 class PresentationController extends Controller
 {
     /**
+     * @Route("/", name="theodo_imagine_formation_home")
+     */
+    public function homeAction(Request $request)
+    {
+        return $this->redirect($this->generateUrl('theodo_imagine_formation_presentation'));
+    }
+
+    /**
      * @Route("/presentation", name="theodo_imagine_formation_presentation")
      * @Template()
      */
